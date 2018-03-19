6f592839a819be8289952fb923a97c2d934f7b99
chetan_ks <chetanks709@gmail.com>
2018-03-18 18:09:46 -0700

    scatter plot

--- src/regression.py
+++ src/regression.py
@@ -1,5 +1,6 @@
 from sklearn import linear_model
 import numpy as np
+import matplotlib.pyplot as plt
 
 X = np.array([200,1300,1500,400,150,1100,900,1500,1450,1100,100,1100,800,300,600])
 y = np.array([1,3,4,2,1,3,3,5,4,3,2,4,3,2,2])
@@ -12,3 +13,6 @@ print(reg.coef_)
 X_test = np.array([750,1000])
 X_test = X_test.reshape((X_test.shape[0],1))
 print(reg.predict(X_test))
+
+plt.scatter(X,y,alpha=0.9)
+plt.show()
